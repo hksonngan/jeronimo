3c4367257c8ddd190a580b163f019f3593706de5
apinzonf <apinzonf@78a4e28a-90cd-11de-9d83-e924b18393b8>
2011-02-02 22:06:02 +0000

    This version performs only one iteration correctly on small models.

---
+++ sjSkeletonizer-0.2.0/include/sjDataIO.h
@@ -0,0 +1,65 @@
+/**
+ * ***** BEGIN GPL LICENSE BLOCK *****
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * The Original Code is Copyright (C) 2007-2010 by Alexander Pinzon Fernandez.
+ * Bogota - Colombia
+ * All rights reserved.
+ *
+ * Author(s): Alexander Pinzón Fernández.
+ *
+ * ***** END GPL LICENSE BLOCK *****
+ */
+
+/** 
+ * @file sjDataIO.h
+ * @brief sjViewer San Jeronimo Viewer Application.
+ * @author Alexander Pinzon Fernandez.
+ * @date 21/01/2011 10:07 a.m.
+ */
+
+#ifndef __SJDATAIO__H__
+#define __SJDATAIO__H__
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <stdexcept>
+#include <cmath>  
+#include "sjDatatype.h"
+
+#include <CGAL/IO/Polyhedron_iostream.h>
+#include <CGAL/IO/Polyhedron_scan_OFF.h> 
+
+using namespace std;
+
+class sjDataIO{
+public:
+	sjDataIO();
+	void setFileName(string a_file_name);
+	string getFileName();
+	void load() throw(exception);
+	void config();
+	double angle3(sjPoint_3 p1, sjPoint_3 p2, sjPoint_3 p3);
+	vector<vector<double>> computeLaplacian();
+	sjPolyhedron getPolyhedronModel();
+private:
+	string file_name;
+	sjPolyhedron polyhedron_model;
+
+};
+#endif //__SJDATAIO__H__
---
+++ sjSkeletonizer-0.2.0/include/sjLaplacianSmoothing.h
@@ -0,0 +1,100 @@
+/**
+ * ***** BEGIN GPL LICENSE BLOCK *****
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * The Original Code is Copyright (C) 2007-2010 by Alexander Pinzon Fernandez.
+ * Bogota - Colombia
+ * All rights reserved.
+ *
+ * Author(s): Alexander Pinzón Fernández.
+ *
+ * ***** END GPL LICENSE BLOCK *****
+ */
+
+/** 
+ * @file sjLaplacianSmoothing.h
+ * @brief sjViewer San Jeronimo Viewer Application.
+ * @author Alexander Pinzon Fernandez.
+ * @date 21/01/2011 10:07 a.m.
+ */
+
+#ifndef __SJLAPLACE_SMOOTH__H__
+#define __SJLAPLACE_SMOOTH__H__
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <stdexcept>
+#include <cmath>  
+
+#include "sjDataType.h"
+using namespace std;
+
+class sjLaplacianSmoothing{
+public:
+	sjLaplacianSmoothing();
+	void setMeshG(sjPolyhedron meshG);
+	void setMatrixL(sjMatrixDouble matrixL);
+	void setMatrixV(sjMatrixDouble matrixV);
+	void setMatrixVprima(sjMatrixDouble matrixVprima);
+	void setMatrixWl(sjVectorDouble matrixWl);
+	void setMatrixWh(sjVectorDouble matrixWh);
+	void setRings(vector<vector< sjVertex_handle> > aRings);
+
+	sjPolyhedron getMeshG();
+	sjMatrixDouble getMatrixL();
+	sjMatrixDouble getMatrixV();
+	sjMatrixDouble getMatrixVprima();
+	sjVectorDouble getMatrixWl();
+	sjVectorDouble getMatrixWh();
+	vector<vector< sjVertex_handle> > getRings();
+	size_t getNsize();
+
+	sjMatrixDouble computeL();
+	void initVertexIndex();
+	sjVectorDouble initRatioMatrixWl(double ratio = 0.001);
+	sjVectorDouble initRatioMatrixWh(double ratio = 1.0);
+	
+	sjVectorDouble updateMatrixWh(sjVectorDouble WH);
+	sjVectorDouble updateMatrixWl(sjVectorDouble WL);
+	sjMatrixDouble multiplyWlxL(sjVectorDouble WL, sjMatrixDouble L);
+	sjVectorDouble multiplyWhxV(sjVectorDouble WH, sjMatrixDouble V, int coord);
+
+	void findRings();
+	double averageFace();
+	sjVectorDouble areaRings();
+
+	void pruebaOpenNL();
+	void initLaplacianSystem();
+	void iterateLaplacianSystem();
+	
+private:
+	sjPolyhedron mesh_G;
+	sjMatrixDouble matrix_L;
+	sjMatrixDouble matrix_V;
+	sjMatrixDouble matrix_Vprima;
+	sjVectorDouble matrix_Wl;
+	sjVectorDouble matrix_Wh;
+	vector<vector< sjVertex_handle> > rings;
+	sjVectorDouble area_rings;
+	sjVectorDouble area_rings_original;
+	double const_wh ;
+	double const_SL ;
+	int iteration;
+};
+
+#endif
